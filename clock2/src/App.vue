d33dc9b6badeb719656384382498f939113fad11
Lee, HuiJu <leemoon63@gmail.com>
2023-05-24 07:47:22 +0900

    add Vue Clock example

---
+++ clock2/src/App.vue
@@ -0,0 +1,23 @@
+<template> 
+  <div id="app"> 
+    <h1>시계</h1> 
+    <MyClock></MyClock> 
+  </div> 
+</template> 
+<script> 
+import MyClock from './MyClock.vue' 
+  
+export default { 
+  name: "App", 
+  components: { MyClock } 
+} 
+</script> 
+<style scoped> 
+  div#app { 
+    padding: 0 30px 30px 30px; 
+    margin: 30px auto; 
+    max-width: 400px; 
+    border: 1px solid #ccc; 
+    box-shadow: 3px 3px 3px #aaa; 
+  } 
+</style>
---
+++ clock2/src/MyClock.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+       {{ time }} 
+    </div>
+</template>
+<script>
+import moment from "moment";
+
+export default {
+    name: "MyClock",
+    data(){
+        return {time: null};
+    },
+    mounted() {
+        let callback = () => this.time = moment().format("HH:mm:ss");
+        callback();
+        setInterval(callback, 1000);
+    },
+}
+</script>
+<style>
+    
+</style>
