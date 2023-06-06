f9eba31e77e06e74bf7f09a9b38e8c108bd3e3b9
Lee, HuiJu <leemoon63@gmail.com>
2023-06-06 23:45:08 +0900

    add Vue todo  project

---
+++ todo1/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app"> 
+    <MyTodo /> 
+    </div>
+</template>
+<script>
+import MyTodo from './MyTodo.vue'
+
+export default {
+  name: "App", 
+  components: { MyTodo }
+}
+</script>
+<style>
+  
+</style>
---
+++ todo1/src/MyTodo.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <h1>할일</h1>
+        <table>
+            <tr><td>ID</td><td>할일</td></tr> 
+            <tr v-for="(todo, index) in todos" v-bind:key="todo.id" v-bind:class="{done: todo.done}"> 
+                <td>{{ todo.id }}</td> 
+                <td>
+                    <input type="checkbox" v-model="todo.done" />
+                    {{ todo.title }}
+                    <span v-on:click="deleteTodo(index)">x</span>
+                </td>
+            </tr>
+        </table>
+        <input type="text" v-model="title" /> 
+        <button type="button" v-on:click="addTodo">추가</button>
+    </div>
+</template>
+<script>
+export default {
+    name: "MyTodo",
+    data(){
+        return {
+            todos:[
+            { id: 1, title: "과제", done: false }, 
+            { id: 2, title: "시험공부", done: false }, 
+            { id: 3, title:"코딩훈련", done: false }
+            ],
+            title: "", 
+            lastId: 3
+        }
+    },
+    methods: { 
+        addTodo: function() { 
+            this.todos.push({id: ++this.lastId, title: this.title}); 
+            this.title = ""; 
+        },
+        deleteTodo(index) { 
+            if (confirm("삭제하시겠습니까?")) 
+                this.todos.splice(index, 1); 
+        }
+    }
+}
+</script>
+<style scoped>
+    h1 { border-bottom: 1px solid gray; } 
+    div { padding: 30px; margin: 30px auto; width: 400px; border: 1px solid #ccc; box-shadow: 3px 3px 3px #aaa; } 
+    table { border-collapse: collapse; margin: 20px 0; width: 100%; } 
+    tr:nth-child(1) { background-color: #eee; text-align: center; } 
+    td { border: 1px solid gray; padding: 6px; } 
+    td:nth-child(1) { text-align: center; width: 30px; }
+    input[type=text] { padding: 5px; margin-right: 5px; width: 300px; } 
+    button { padding: 0.3em 1.5em; } 
+    span { font-weight: bold; margin-left: 10px; cursor: pointer; float: right;}
+    tr.done { background-color: #f8f8f8; color: #bbb; text-decoration: line-through; } 
+    input[type=checkbox] { accent-color: #bbb;}
+</style>
