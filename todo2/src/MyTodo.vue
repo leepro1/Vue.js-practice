229987f82cf2320adbfef9de004a611fdae3b127
Lee, HuiJu <leemoon63@gmail.com>
2023-06-09 20:30:55 +0900

    feat: input date_Todo

--- todo2/src/MyTodo.vue
+++ todo2/src/MyTodo.vue
@@ -2,9 +2,10 @@
     <div id="MyTodo"> 
         <h1>할 일</h1> 
         <table> 
-            <tr><td>ID</td><td>할일</td></tr> 
+            <tr><td>ID</td><td>기한</td><td>할일</td></tr> 
             <tr v-for="todo in todos" v-bind:key="todo.id" v-bind:class="{done: todo.done}">           
                 <td>{{ todo.id }}</td> 
+                <td>{{ todo.dueDate }}</td>  
                 <td>
                     <input type="checkbox" v-model="todo.done" @click="toggleTodo(todo)"/> 
                     {{ todo.title }}
@@ -12,7 +13,8 @@
                 </td> 
             </tr> 
         </table> 
-        <input type="text" v-model="title" />
+        <input type="text" v-model="title" placeholder="할일을 입력하세요"/>
+        <input type="date" v-model="dueDate" />
         <button type="button" @click="addTodo">추가</button>
     </div>
 </template>
@@ -25,7 +27,9 @@ export default {
     data() { 
         return { 
             todos: [ ], 
-            title: ""
+            title: "",
+            done: false,
+            dueDate:""
         } 
     }, 
     mounted() { // 컴포넌트가 처음 화면에 보이게 될 때, 자동으로 호출되는 메소드 
@@ -42,10 +46,11 @@ export default {
         },
         async addTodo() { 
             try { 
-                const todo = {id: 0, title: this.title}; //id가 AI이기 때문에 자동으로 결정된다.
+                const todo = {id: 0, dueDate: this.dueDate, done: this.done, title: this.title}; //id가 AI이기 때문에 자동으로 결정된다.
                 await axios.post("/todos", todo); // 새 todo를 백엔드에 등록한다 
                 this.reloadTodos(); // todo 목록 다시 로드 
                 this.title = ""; 
+                this.dueDate = "";
             } catch (error) { 
                 alert('저장 에러: ' + (error instanceof Error ? error.message : error)); 
             } 
@@ -72,13 +77,15 @@ export default {
 }
 </script>
 <style scoped>
-    div#MyTodo { padding: 30px; margin: 30px auto; width: 400px; border: 1px solid #ccc; box-shadow: 3px 3px 3px #aaa; } 
+    div#MyTodo { padding: 30px; margin: 30px auto; width: 500px; border: 1px solid #ccc; box-shadow: 3px 3px 3px #aaa; } 
     h1 { border-bottom: 1px solid gray; } 
     table { border-collapse: collapse; margin: 20px 0; width: 100%; } 
     tr:nth-child(1) { background-color: #eee; text-align: center; } 
     td { border: 1px solid gray; padding: 6px; } 
     td:nth-child(1) { text-align: center; width: 30px;}
-    input[type=text] { padding: 5px; margin-right: 5px; width: 300px; } 
+    td:nth-child(2) { text-align: center; width: 100px;}
+    input[type=text] { padding: 5px; margin-right: 5px; width: 290px; } 
+    input[type=date] { padding: 5px; margin-right: 5px; width: 100px; } 
     button { padding: 0.3em 1.5em; }
     span { font-weight: bold; margin-left: 10px; cursor: pointer; float: right; }
     tr.done { background-color: #f8f8f8; color: #bbb; text-decoration: line-through; } 
