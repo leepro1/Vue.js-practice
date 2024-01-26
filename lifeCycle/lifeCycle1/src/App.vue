b2cee52bc258ef1498e5367f1ab35cf28fa1799f
Huiju Lee <leemoon63@gmail.com>
2024-01-26 22:53:20 +0900

    Vue.js LifeCycle Test

---
+++ lifeCycle/lifeCycle1/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    {{ count }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+        count: 0,
+      }
+  },
+  beforeCreate(){
+    console.log("lifeCycle is beforeCreate", this.count)
+  },
+  created(){
+    console.log("lifeCycle is created", this.count)
+  }
+}
+</script>
