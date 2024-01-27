47a03cf89c38d0efdb1f14593c370a666c62c2fa
Huiju Lee <leemoon63@gmail.com>
2024-01-27 17:26:51 +0900

    Vue.js LifeCycle Test2

--- lifeCycle/lifeCycle1/src/App.vue
+++ lifeCycle/lifeCycle1/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     {{ count }}
+    <h1>라이프사이클 test</h1>
   </div>
 </template>
 
@@ -13,9 +14,21 @@ export default {
   },
   beforeCreate(){
     console.log("lifeCycle is beforeCreate", this.count)
+    this.test()
   },
   created(){
     console.log("lifeCycle is created", this.count)
+  },
+  beforeMount(){
+    console.log("lifeCycle is beforeMount", document.querySelector("h1"))
+  },
+  mounted(){
+    console.log("lifeCycle is mounted", document.querySelector("h1"))
+  },
+  methods: {
+    test(){
+      console.log("test 함수입니다.")
+    }
   }
 }
 </script>
