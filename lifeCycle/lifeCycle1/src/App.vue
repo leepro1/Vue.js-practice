e16f4965cfbaf5fda74ef98e84fed2d25951746a
Huiju Lee <leemoon63@gmail.com>
2024-01-29 20:16:45 +0900

    UI 디렉티브_선언적 렌더링, class-style 바인딩

--- lifeCycle/lifeCycle1/src/App.vue
+++ lifeCycle/lifeCycle1/src/App.vue
@@ -1,34 +1,34 @@
 <template>
-  <div>
-    {{ count }}
-    <h1>라이프사이클 test</h1>
-  </div>
+  <div>{{ rawHtml }}</div>
+  <div>{{ rawHtml2 }}</div>
+  <h1 v-html="rawHtml2"></h1>
+  
+  <h2 v-bind:class="{active:isActive}">액티브</h2>
+  <h2 :class="{active:isActive}">액티브</h2>
+  <button @click="change">버튼</button>
+
+  <h3 :style="{color:'blue'}">스타일 바인딩 테스트</h3>
 </template>
 
 <script>
 export default {
   data() {
-      return {
-        count: 0,
-      }
-  },
-  beforeCreate(){
-    console.log("lifeCycle is beforeCreate", this.count)
-    this.test()
-  },
-  created(){
-    console.log("lifeCycle is created", this.count)
-  },
-  beforeMount(){
-    console.log("lifeCycle is beforeMount", document.querySelector("h1"))
-  },
-  mounted(){
-    console.log("lifeCycle is mounted", document.querySelector("h1"))
+    return {
+      rawHtml: '이것은 텍스트입니다.',
+      rawHtml2: '<span style="color:red">이것은 빨간색입니다.</span>',
+      isActive: false,
+    }
   },
   methods: {
-    test(){
-      console.log("test 함수입니다.")
+    change() {
+      this.isActive = !this.isActive
     }
   }
 }
 </script>
+
+<style scoped>
+h2.active {
+  color:aquamarine
+}
+</style>
